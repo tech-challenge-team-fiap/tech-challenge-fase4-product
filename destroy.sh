df4fc2e5132c7f0dcb9afb5e4d355a9678bcc3cb
thales.jolo <thales.alexandre.jolo@gmail.com>
2024-05-11 12:13:13 -0300

    Adjust

--- destroy.sh
+++ destroy.sh
@@ -3,7 +3,7 @@
 # Destroy infrastructure using Terraform
 cd infra
 terraform init
-terraform destroy -auto-approve -var "aws_access_key=${AWS_ACCESS_KEY}" -var "aws_secret_key=${AWS_SECRET_KEY}"
+terraform destroy -auto-approve -var "aws_access_key=${AWS_ACCESS_KEY_ID}" -var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}"
 cd ..
 
 echo "Destroyed completed!"
