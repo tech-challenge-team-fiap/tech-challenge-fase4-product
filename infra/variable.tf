7ad0e9b5d4e7b1a39d18a6e6fcccb64d2646a013
thales.jolo <thales.alexandre.jolo@gmail.com>
2024-05-13 20:17:08 -0300

    Adjust cloud deploy

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,2 +1,15 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-2"
+}
